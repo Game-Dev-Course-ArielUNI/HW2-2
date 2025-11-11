72768cdbb37f799b067dd3c739a686f8767e133b
yousefmasarwa97 <93148482+yousefmasarwa97@users.noreply.github.com>
2025-11-11 13:25:40 +0200

    Update AdaptiveCamera.cs

--- Assets/Scripts/AdaptiveCamera.cs
+++ Assets/Scripts/AdaptiveCamera.cs
@@ -1,15 +1,31 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
-public class AdaptiveCamera : MonoBehaviour
+public class adaptivecamera : MonoBehaviour
 {
-    [SerializeField] private float baseAspect = 16f / 9f;
-    [SerializeField] private float baseSize = 5f;
+    [Tooltip("The base size of the camera relative to the initial width/height)")]
+    public float baseOrthographicSize;
+
+    [Tooltip("The initial width-to-height ratio the game was designed for)")]
+    public float referenceAspect = 16f / 9f;
+
+    private Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        AdjustCamera();
+    }
 
     void Update()
     {
-        Camera cam = GetComponent<Camera>();
+
+        AdjustCamera();
+    }
+
+    void AdjustCamera()
+    {
         float currentAspect = (float)Screen.width / Screen.height;
-        cam.orthographicSize = baseSize * (baseAspect / currentAspect);
+        cam.orthographicSize = baseOrthographicSize * (referenceAspect / currentAspect);
     }
 }
